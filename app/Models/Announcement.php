2bfaf3354f87acbb5f3f42b8704b6eedb6982927
ikyy <182581582+rizukiii@users.noreply.github.com>
2025-01-06 08:43:39 +0700

    set model table announcement

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Announcement extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'announcements';
+    protected $primarykey = 'id';
+    protected $fillable = [
+        'image',
+        'title',
+        'description',
+        'date'
+    ];
+
+    protected function casts(){
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime'
+        ];
+    }
 }
